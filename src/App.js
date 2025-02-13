ebdcd7faee88070326373732632f83a87c289304
Thebadking00 <al222311228@gmail.com>
2025-02-12 22:26:31 -0600

    Cambio de fondo main

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Primer App en AWS  
+        Esta es la rama main
         </p>
         
       </header>
